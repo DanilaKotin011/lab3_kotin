e709e75f281e36770fce811d12173d610ce9da5b
DanilaKotin011 <kotin.02@mail.ru>
2022-03-02 14:44:34 +0700

    create lab3

---
+++ lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace lab2
+{    class Program
+    {
+        static int Main(string[] args)
+        {
+            RomanNumber testr = new RomanNumber(10);
+            Console.WriteLine($"Num1 = {testr}");
+            RomanNumber testr2 = new RomanNumber(6);
+            Console.WriteLine($"Num2 = {testr2}");
+            Console.WriteLine($"\nAdd = {testr + testr2}");
+            Console.WriteLine($"Sub = {testr - testr2}");
+            Console.WriteLine($"Mul = {testr * testr2}");
+            Console.WriteLine($"Div = {testr / testr2}");
+
+            Console.WriteLine($"\nOriginal is {testr2.ToString()}");
+            var clon = (RomanNumber)testr2.Clone();
+            Console.WriteLine($"Copy is {clon.ToString()}\n");
+
+
+            RomanNumber sort1 = new RomanNumber(12);
+            RomanNumber sort2 = new RomanNumber(5);
+            RomanNumber sort3 = new RomanNumber(10);
+            RomanNumber[] mas = { sort1, sort2, sort3 };
+            Console.WriteLine($"\nOriginal massive");
+            for (int i = 0; i < 3; i++) Console.WriteLine($"{mas[i].ToString()}");
+            Array.Sort(mas);
+            Console.WriteLine($"\nSorted massive");
+            for (int i = 0; i < 3; i++) Console.WriteLine($"{mas[i].ToString()}");
+            return 0;
+
+        }
+    }
+}
---
+++ lab2/RomanNumberException .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class RomanNumberException : Exception
+    {
+        //Overriding the Message property
+        public override string Message
+        {
+            get
+            {
+                return "false number";
+            }
+        }
+    }
+}
